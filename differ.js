a06496f7676dad3615c355a457f23acb08a8d728
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-14 15:11:02 -0500

    added breakpoints

--- differ.js
+++ differ.js
@@ -153,3 +153,22 @@
 // console.log(eitherStringIncluded("Question?", "Answer", "?")); //=> true
 // console.log(eitherStringIncluded("I love programming", "apple", "potato")); //=> false
 // console.log(eitherStringIncluded("Dance party!", "?", "what")); //=> false
+
+
+
+// Pig Latin - Slice Research****************************************************
+
+// So the two rules for our version of Pig Latin are:
+
+// 1. For words that start with a vowel, add 'yay' to the end of the word.
+// 2. For words that start with a non-vowel, move all letters that come
+// **before the first vowel** to the **end of the word** then add 'ay'
+
+function pigLatinWord(word) {
+    // your code here...
+  };
+
+  console.log(pigLatinWord("apple")); //=> "appleyay"
+  console.log(pigLatinWord("eat")); //=> "eatyay"
+  console.log(pigLatinWord("banana")); //=> "ananabay"
+  console.log(pigLatinWord("trash")); //=> "ashtray"
