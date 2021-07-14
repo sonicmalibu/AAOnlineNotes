f6dfdd9e3c0b32ea46386f267156e26662b2ff19
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-14 14:42:31 -0500

    array index of

--- differ.js
+++ differ.js
@@ -77,25 +77,51 @@
 
 
 // Fizz Buzz***********************************
-function fizzBuzz(max) {
+// function fizzBuzz(max) {
 
-    for (let i = 0; i < max; i++) {
-       if (i % 3 === 0 && i % 5 !== 0) {
-           console.log(i);
-       } else if (i % 5 === 0 && i % 3 !== 0) {
-           console.log(i);
-       }
+//     for (let i = 0; i < max; i++) {
+//        if (i % 3 === 0 && i % 5 !== 0) {
+//            console.log(i);
+//        } else if (i % 5 === 0 && i % 3 !== 0) {
+//            console.log(i);
+//        }
+//     }
+
+// }
+
+
+
+// fizzBuzz(20); // prints out:
+// 3
+// 5
+// 6
+// 9
+// 10
+// 12
+// 18
+
+
+function wordWithinArray(array, word) {
+    if (array.indexOf(word) >= 0) {
+        return true;
+    } else {
+        return false;
     }
 
-}
+};
 
+console.log(
+  wordWithinArray(["apple", "banana", "caramel", "chocolate"], "apple")
+); //=> true
 
+console.log(wordWithinArray(
+  ["dog", "cat", "camel", "bird"], "camel")
+); //=> true
 
-fizzBuzz(20); // prints out:
-3
-5
-6
-9
-10
-12
-18
+console.log(
+  wordWithinArray(["apple", "banana", "caramel", "chocolate"], "pineapple")
+); //=> false
+
+console.log(wordWithinArray(
+  ["dog", "cat", "camel", "bird"], "panther")
+); //=> false
