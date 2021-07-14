0c2177582a8749d8755e0c922af01495ddfff067
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-14 15:01:09 -0500

    string includes

--- differ.js
+++ differ.js
@@ -136,3 +136,20 @@
 //   echo("Mom!"); // => prints "MOM! ... Mom! ... mom!"
 //   echo("hey"); // => prints "HEY ... hey ... hey"
 //   echo("JUMp"); // => prints "JUMP ... JUMp ... jump"
+
+
+
+// String Included (*)******************************************************
+// function eitherStringIncluded(sentence, word1, word2) {
+//     if (sentence.includes(word1)) {
+//         return true;
+//     } else if (sentence.includes(word2)) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// console.log(eitherStringIncluded("how now brown cow?", "panther", "cow")); //=> true
+// console.log(eitherStringIncluded("Question?", "Answer", "?")); //=> true
+// console.log(eitherStringIncluded("I love programming", "apple", "potato")); //=> false
+// console.log(eitherStringIncluded("Dance party!", "?", "what")); //=> false
