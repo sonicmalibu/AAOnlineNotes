6b194492bec362dd11527689d100173fbba4eb80
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-14 21:21:59 -0500

    unknown

--- differ.js
+++ differ.js
@@ -292,3 +292,16 @@
 // console.log(isPrime(11)); // => true
 // console.log(isPrime(9)); // => false
 // console.log(isPrime(2017)); // => true
+
+
+// Reverse String***********************************************
+function reverseString(str) {
+    console.log(str.length);
+    for (let i = 0; i = str.length; i++) {
+
+    }
+}
+
+
+console.log(reverseString('fish')); // 'hsif'
+console.log(reverseString('marathon')); // 'nohtaram'
