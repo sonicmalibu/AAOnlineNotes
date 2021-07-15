ac495450e911e80cf91dc903c3d26c79bf3af751
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-15 12:21:07 -0500

    my includes

--- differ.js
+++ differ.js
@@ -350,3 +350,25 @@
 // 5
 // 10
 // 15
+
+
+
+// My Includes********************************************************
+function myIncludes(arr, target) {
+    // return arr.includes(target);
+    for (let i = 0; i < arr.length; i++) {
+        let index = arr[i];
+        if (index === target) {
+            return true;
+        }
+
+    }
+    return false;
+}
+
+
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'c')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'a')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'z')); // false
+console.log(myIncludes([43, -7, 11, 13], 11)); // true
+console.log(myIncludes([43, -7, 11, 13], 1)); // false
