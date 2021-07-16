aa9300cf9cc6b16901740a876158799de745aa21
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-16 18:57:45 -0500

    Pit Pat

--- differ.js
+++ differ.js
@@ -572,3 +572,19 @@
 // console.log(lastVowel('dog')); // 'o'
 // console.log(lastVowel('conventional')); // 'a'
 // console.log(lastVowel('rhythm')); // null
+
+
+// Pit Pat****************************************************
+// function pitPat(max) {
+//     let arr = [];
+//     for (let i = 1; i <= max; i++) {
+//         if ((i %  4 === 0 || i % 6 === 0) && !(i %  4 === 0 && i % 6 === 0)) {
+//             arr.push(i);
+//         }
+
+//     }
+//     return arr;
+// }
+
+// console.log(pitPat(18)); // [ 4, 6, 8, 16, 18 ]
+// console.log(pitPat(30)); // [ 4, 6, 8, 16, 18, 20, 28, 30 ]
