1d995caff42d8c8058245cf63a927dbb61f89227
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-16 10:50:00 -0500

    My Index Of

--- differ.js
+++ differ.js
@@ -413,15 +413,22 @@
 
 
 // My Index Of***************************************************
-function myIndexOf(arr, target) {
+// function myIndexOf(arr, target) {
+// for (let i = 0; i < arr.length; i++) {
+//     let searcher = arr[i];
+//     if (target === searcher) {
+//         return i;
+//     }
 
-}
+// }
+// return -1;
+// }
 
 
 
 
-console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
-console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
-console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
-console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
-console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
+// console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
+// console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
+// console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
+// console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
+// console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
