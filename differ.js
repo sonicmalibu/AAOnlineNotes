a156f402d8b0cd57951c67b11a24e257490c5e79
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-16 19:06:54 -0500

    stopping point for the evening

--- differ.js
+++ differ.js
@@ -607,3 +607,21 @@
 // console.log(removeLastVowel('better')); // 'bettr'
 // console.log(removeLastVowel('graph')); // 'grph'
 // console.log(removeLastVowel('thy')); // 'thy'
+
+// Pairs Maker**************************************************
+function pairsMaker(arr) {
+
+}
+
+console.log(pairsMaker(['a', 'b', 'c', 'd'])); // =>
+// [ [ 'a', 'b' ],
+//   [ 'a', 'c' ],
+//   [ 'a', 'd' ],
+//   [ 'b', 'c' ],
+//   [ 'b', 'd' ],
+//   [ 'c', 'd' ] ]
+
+console.log(pairsMaker(['Rosemary', 'Alex', 'Connor'])); // =>
+// [ [ 'Rosemary', 'Alex' ],
+//   [ 'Rosemary', 'Connor' ],
+//   [ 'Alex', 'Connor' ] ]
