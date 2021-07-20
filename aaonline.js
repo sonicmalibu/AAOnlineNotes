da1782c7bd81b68f9efd27f245640a94966b45b0
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-20 12:45:36 -0500

    optimizing notes

--- aaonline.js
+++ aaonline.js
@@ -1,110 +1,110 @@
-function smallNumber() {
-    console.log('this is a small number')
-}
-function bigNumber() {
-    console.log('this is a big number')
-}
+// function smallNumber() {
+//     console.log('this is a small number')
+// }
+// function bigNumber() {
+//     console.log('this is a big number')
+// }
 
 
-function numberSeparator(number) {
-    if (number > 100) {
-        bigNumber();
-    } else {
+// function numberSeparator(number) {
+//     if (number > 100) {
+//         bigNumber();
+//     } else {
 
-        smallNumber();
-    }
-};
+//         smallNumber();
+//     }
+// };
 
-let alphabet = [
-    "a", "b", "c"
-];
-console.log(alphabet);
-console.log([4, 7, 9].length);
+// let alphabet = [
+//     "a", "b", "c"
+// ];
+// console.log(alphabet);
+// console.log([4, 7, 9].length);
 
-let numbersAndLetters = ["b", "z", 17];
-console.log(numbersAndLetters[2]);
-console.log(numbersAndLetters[0]);
+// let numbersAndLetters = ["b", "z", 17];
+// console.log(numbersAndLetters[2]);
+// console.log(numbersAndLetters[0]);
 
-let testArray = [4, 7, 9];
-let finalIndex = testArray.length - 1; // => (3 - 1) = 2
-console.log(testArray[finalIndex]); // => 9
+// let testArray = [4, 7, 9];
+// let finalIndex = testArray.length - 1; // => (3 - 1) = 2
+// console.log(testArray[finalIndex]); // => 9
 
-function testme() {
-    console.log("test me please!");
-}
+// function testme() {
+//     console.log("test me please!");
+// }
 
-testme(); //=> prints "test me please!"
+// testme(); //=> prints "test me please!"
 
-function averageofFour(num1, num2, num3, num4) {
-    // 1 sum the 4 incoming parameters
-    // 2 divide the sum by four
-    // 3 return my new average
+// function averageofFour(num1, num2, num3, num4) {
+//     // 1 sum the 4 incoming parameters
+//     // 2 divide the sum by four
+//     // 3 return my new average
 
-    let sum = num1 + num2 + num3 + num4;
-    let average = sum / 4;
-    return average;
-}
-//console.log(averageofFour(10, 10, 15, 5)); //=>10
-console.log(averageofFour(3, 10, 11, 4)); // => 7
+//     let sum = num1 + num2 + num3 + num4;
+//     let average = sum / 4;
+//     return average;
+// }
+// //console.log(averageofFour(10, 10, 15, 5)); //=>10
+// console.log(averageofFour(3, 10, 11, 4)); // => 7
 
-if (true === true) {
-    console.log("This is true!");
-} else {
-    console.log("this is false!");
-}
-
-
-let index = 0;
-
-while (index < 10) {
-    console.log("hello world");
-    index++;
-}
+// if (true === true) {
+//     console.log("This is true!");
+// } else {
+//     console.log("this is false!");
+// }
 
 
-console.log('bootcamp'.indexOf('camp'));
-console.log('run'['run'.length - 1]);
-console.log('bootcamp'.indexOf('t'));
-console.log('bootcamp'.indexOf('o'));
-console.log('bootcamp'.indexOf('x'));
-console.log('run'[3]);
+// let index = 0;
 
-function addThreeNumbers(num1, num2, num3) {
-    return num1 + num2 + num3;
-}
-addThreeNumbers(1, 2, 3, 4);
-
-function addThreeNumbers(num1, num2, num3) {
-    return num1 + num2 + num3;
-  }
-
-  addThreeNumbers(1, 2);
-
-  function isCool(string) {
-    let coolSentence = string + " is cool!"
-    console.log(coolSentence)
-    return "function return"
-  }
-
-  // Call the above function passing in the below arguments:
-  console.log(isCool("Music"));// Music
-  console.log(isCool("JavaScript"));// JavaScript
-  console.log(isCool("The World"));// The world
-
-  let age = 30;
-
-if (age > 30) {
-  console.log("older than 30");
-} else {
-  console.log("younger than 30");
-}
+// while (index < 10) {
+//     console.log("hello world");
+//     index++;
+// }
 
 
-function sumArray(array) {
-    // your code here...
+// console.log('bootcamp'.indexOf('camp'));
+// console.log('run'['run'.length - 1]);
+// console.log('bootcamp'.indexOf('t'));
+// console.log('bootcamp'.indexOf('o'));
+// console.log('bootcamp'.indexOf('x'));
+// console.log('run'[3]);
 
-}
+// function addThreeNumbers(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
+// addThreeNumbers(1, 2, 3, 4);
+
+// function addThreeNumbers(num1, num2, num3) {
+//     return num1 + num2 + num3;
+//   }
+
+//   addThreeNumbers(1, 2);
+
+//   function isCool(string) {
+//     let coolSentence = string + " is cool!"
+//     console.log(coolSentence)
+//     return "function return"
+//   }
+
+//   // Call the above function passing in the below arguments:
+//   console.log(isCool("Music"));// Music
+//   console.log(isCool("JavaScript"));// JavaScript
+//   console.log(isCool("The World"));// The world
+
+//   let age = 30;
+
+// if (age > 30) {
+//   console.log("older than 30");
+// } else {
+//   console.log("younger than 30");
+// }
 
 
-console.log(sumArray([5, 6, 4])); // => 15
-console.log(sumArray([7, 3, 9, 11])); // => 30
+// function sumArray(array) {
+//     // your code here...
+
+// }
+
+
+// console.log(sumArray([5, 6, 4])); // => 15
+// console.log(sumArray([7, 3, 9, 11])); // => 30
--- app.js
+++ app.js
@@ -1,84 +1,84 @@
-let arr = ["a", "e", "i", "o", "u"];
-let num1 = "10";
-let letter = "e";
+// let arr = ["a", "e", "i", "o", "u"];
+// let num1 = "10";
+// let letter = "e";
 
-console.log(arr.includes(num1));
-console.log(arr.includes(letter));
+// console.log(arr.includes(num1));
+// console.log(arr.includes(letter));
 
-function countVowels(word) {
-    // your code here...
-    let arr = ["a", "e", "i", "o", "u"];
-    let counter = 0;
-    let i = 0;
-    while (i < word.length) {
-        let letter = word[i];
-        if (arr.includes(letter)){
+// function countVowels(word) {
+//     // your code here...
+//     let arr = ["a", "e", "i", "o", "u"];
+//     let counter = 0;
+//     let i = 0;
+//     while (i < word.length) {
+//         let letter = word[i];
+//         if (arr.includes(letter)){
 
-         counter++; }
+//          counter++; }
 
-        i++;
-
-
-    }
-
-return counter;
-};
-
-console.log(countVowels("bootcamp")); // => 3
-console.log(countVowels("apple")); // => 2
-console.log(countVowels("pizza")); // => 2
+//         i++;
 
 
-// So the two rules for our version of Pig Latin are:
+//     }
 
-// 1. For words that start with a vowel, add 'yay' to the end of the word.
-// 2. For words that start with a non-vowel, move all letters that come
-// **before the first vowel** to the **end of the word** then add 'ay'
+// return counter;
+// };
 
-function pigLatinWord(word) {
-    // your code here...
-    //Use slice() method
-    //Find words that begin with vowl
-    //add -yay to those words
-    //don't forget string.includes method
-
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-    let str1 = 0;
+// console.log(countVowels("bootcamp")); // => 3
+// console.log(countVowels("apple")); // => 2
+// console.log(countVowels("pizza")); // => 2
 
 
+// // So the two rules for our version of Pig Latin are:
 
-  }
+// // 1. For words that start with a vowel, add 'yay' to the end of the word.
+// // 2. For words that start with a non-vowel, move all letters that come
+// // **before the first vowel** to the **end of the word** then add 'ay'
 
-  console.log(pigLatinWord("apple")); //=> "appleyay"
-  console.log(pigLatinWord("eat")); //=> "eatyay"
-  console.log(pigLatinWord("banana")); //=> "ananabay"
-  console.log(pigLatinWord("trash")); //=> "ashtray"
+// function pigLatinWord(word) {
+//     // your code here...
+//     //Use slice() method
+//     //Find words that begin with vowl
+//     //add -yay to those words
+//     //don't forget string.includes method
 
-  let index = 0;
-  let count = 0;
-  while (index < word.length) {
-    let char = word[index];
-    if (char === "e" || char === "E") {
-      count += 1;
-    }
-    index++;
-    return count;
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let str1 = 0;
 
 
 
-function eCounter(word) {
-        let count = 0;
+//   }
 
-        for (let index = 0; index < word.length; index++) {
-          let char = word[index];
-          if (char === "e" || char === "E") {
-            count += 1;
-          }
-        }
+//   console.log(pigLatinWord("apple")); //=> "appleyay"
+//   console.log(pigLatinWord("eat")); //=> "eatyay"
+//   console.log(pigLatinWord("banana")); //=> "ananabay"
+//   console.log(pigLatinWord("trash")); //=> "ashtray"
 
-        return count;
-      };
+//   let index = 0;
+//   let count = 0;
+//   while (index < word.length) {
+//     let char = word[index];
+//     if (char === "e" || char === "E") {
+//       count += 1;
+//     }
+//     index++;
+//     return count;
 
-      console.log(eCounter("apple")); // => 1
-      console.log(eCounter("appleapple")); // => 2
-      console.log(eCounter("Appleee")); // => 3
+
+
+// function eCounter(word) {
+//         let count = 0;
+
+//         for (let index = 0; index < word.length; index++) {
+//           let char = word[index];
+//           if (char === "e" || char === "E") {
+//             count += 1;
+//           }
+//         }
+
+//         return count;
+//       };
+
+//       console.log(eCounter("apple")); // => 1
+//       console.log(eCounter("appleapple")); // => 2
+//       console.log(eCounter("Appleee")); // => 3
