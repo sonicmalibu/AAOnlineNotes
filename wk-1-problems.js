df2f4f5cd1ca3ba84af58c4e4d441a8adc2a010e
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-20 12:39:29 -0500

    min value

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -634,3 +634,20 @@
 // // [ [ 'Rosemary', 'Alex' ],
 // //   [ 'Rosemary', 'Connor' ],
 // //   [ 'Alex', 'Connor' ] ]
+
+
+// Min Value (*)***************************************************************
+function minValue(nums) {
+    let min = null;
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (min === null || num < min) {
+            min = num;
+        }
+    }
+    return min;
+}
+
+console.log(minValue([4, 6, 3, 5, 2, 4])); // 2
+console.log(minValue([-2, -3, -7, 3 ])); // -7
+console.log(minValue([])); // null
