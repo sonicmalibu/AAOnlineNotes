c52d63721353964ba17192e572dcf1721ca67dbf
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-20 21:34:29 -0500

    Rotate Right

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -697,16 +697,23 @@
 
 // Rotate Right*******************************************************************
 let rotateRight = function(array, num) {
+    let copy = array.slice();
+
+    for (let i = 0; i < num; i++) {
+        let el = copy.pop();
+        copy.unshift(el);
+    }
+
+    return copy;
+
+};
 
 
-}
 
 
-
-
-// let arr = ['a', 'b', 'c', 'd', 'e'];
-// console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
-// // console.log(arr); // [ 'a', 'b', 'c', 'd', 'e' ]
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
+console.log(arr); // [ 'a', 'b', 'c', 'd', 'e' ]
 
 let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
 console.log(rotateRight(animals, 3)); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
