b004aa69a4be8499396774bdef0615839e0d1051
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-21 14:52:39 -0500

    Least Common Multiple

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -770,3 +770,18 @@
 //   console.log(pigLatinWord("eat")); //=> "eatyay"
 //   console.log(pigLatinWord("banana")); //=> "ananabay"
 //   console.log(pigLatinWord("trash")); //=> "ashtray"
+
+
+// Least Common Multiple***********************************************************
+// function leastCommonMultiple(num1, num2) {
+//     for (let i = 1; i <= (num1 * num2); i++) {
+//         if (i % num1 === 0 && i % num2 === 0) {
+//             return i;
+//         }
+
+//     }
+// }
+
+// console.log(leastCommonMultiple(4, 6)); // 12
+// console.log(leastCommonMultiple(3, 5)); // 15
+// console.log(leastCommonMultiple(2, 10)); // 10
