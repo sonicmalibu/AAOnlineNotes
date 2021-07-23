359e73b492fdf7d15f232b50a11d60467796e98c
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-23 11:03:15 -0500

    Max Value

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -887,3 +887,25 @@
 
 // console.log(removeEWords('What time is it everyone?')); // 'What is it'
 // console.log(removeEWords('Enter the building')); // 'building'
+
+
+// Max Value**********************************************************************
+// let maxValue = function(nums) {
+//     let max = null;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         let num = nums[i];
+//         if (max === null || num > max) {
+//             max = num;
+//         }
+//     }
+
+//     return max;
+// };
+
+
+
+
+// console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
+// console.log(maxValue([-2, -3, -7, 3 ])); // 3
+// console.log(maxValue([])); // null
