b1b090b43855ee2ca47242889db7ae6d1d5f10bd
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-23 11:30:05 -0500

    Two Sum Recall

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -909,3 +909,27 @@
 // console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
 // console.log(maxValue([-2, -3, -7, 3 ])); // 3
 // console.log(maxValue([])); // null
+
+
+// Two Sum Recall*****************************************************************
+// function twoSum(arr, target) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let num1 = arr[i];
+
+//             for (let j = i + 1; j < arr.length; j++) {
+//                 let num2 = arr[j];
+
+//                 if (num1 + num2 === target) {
+//                     return true;
+//                 }
+
+//             }
+
+//     }
+//     return false;
+// }
+
+// console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
+// console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
+// console.log(twoSum([4, 6, 2, 3], 8)); // true
+// console.log(twoSum([4, 6, 2, 3], 11)); // false
