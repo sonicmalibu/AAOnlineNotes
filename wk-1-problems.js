3b5a66e3e75eef370e9f8af6e7b809d675836ea2
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-26 11:14:09 -0500

    Choose Primes

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -1060,5 +1060,5 @@
 
 
 
-console.log(choosePrimes([36, 48, 9, 13, 19])); // [ 13, 19 ]
-console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
+// console.log(choosePrimes([36, 48, 9, 13, 19])); // [ 13, 19 ]
+// console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
