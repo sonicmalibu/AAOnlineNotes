ab7f4dfa54f1a04fa125f77dd75162ede09f4282
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-26 14:54:09 -0500

    Remove Last Vowel Recall

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -1134,3 +1134,23 @@
 
 // console.log(productWithReduce([10, 3, 5, 2])); // 300
 // console.log(productWithReduce([4, 3])); // 12
+
+
+
+// Remove Last Vowel***************************************************************
+// function removeLastVowel(word) {
+//     let vowels = 'aeiouAEIOU';
+
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         let letter = word[i];
+//         if (vowels.includes(letter)) {
+//             return word.slice(0, i) + word.slice(i + 1);
+//         }
+//     }
+//     return word;
+// }
+
+// console.log(removeLastVowel('bootcamp')); // 'bootcmp'
+// console.log(removeLastVowel('better')); // 'bettr'
+// console.log(removeLastVowel('graph')); // 'grph'
+// console.log(removeLastVowel('thy')); // 'thy'
