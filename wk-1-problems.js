09b283968b058b9acd2adf8dc60fab1a7e5d306b
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-26 15:59:09 -0500

    Uncompressed

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -1206,3 +1206,22 @@
 // console.log(containsWord('They are great', 'they')); // true
 // console.log(containsWord('caterpillars are great animals', 'cat')); // false
 // console.log(containsWord('Cast the net', 'internet')); // false
+
+
+
+// Uncompress*********************************************************************
+// function uncompress(str) {
+//     let word = '';
+//     for (let i = 0; i < str.length; i+=2) {
+//         let letter = str[i];
+//         let num = Number(str[i + 1]);
+//         for (let j = 0; j < num; j+=1) {
+//             word += letter;
+//         }
+//     }
+//     return word;
+// }
+
+// console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+// console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+// console.log(uncompress('b1o2t1')); // 'boot'
