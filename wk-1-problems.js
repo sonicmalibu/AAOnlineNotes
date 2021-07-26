a7060a9178a9ae32122295a8530865066143950c
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-26 10:14:17 -0500

    Reverse String

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -947,3 +947,19 @@
 
 // console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
 // console.log(reverseSentence('follow the yellow brick road')); // 'road brick yellow the follow'
+
+
+
+// Reverse Sentence*****************************************************************
+// function reverseSentence(sentence) {
+//     let words = sentence.split(' ');
+//     let newWords = [];
+//     for (let i = words.length - 1; i >= 0; i--) {
+//         newWords.push(words[i]);
+//     }
+//     let newSentence = newWords.join(' ');
+//     return newSentence;
+// };
+
+// console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
+// console.log(reverseSentence('follow the yellow brick road')); // 'road brick yellow the follow'
