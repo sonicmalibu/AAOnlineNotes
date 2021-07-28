52e8c2192d9db68a74fe7c946e2bd27f1ab613c1
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-28 10:56:41 -0500

    sum for each

---
+++ wk-2-problems.js
@@ -0,0 +1,13 @@
+// Sum For Each**********************************************************************************
+// function sumForEach(arr) {
+//     let sum = 0;
+//     arr.forEach(function(num) {
+//         sum += num;
+//     })
+//     return sum;
+// }
+
+
+// console.log(sumForEach([4, 3, -1, 10])); // 16
+// console.log(sumForEach([6, 7, 2])); // 15
+// console.log(sumForEach([])); // 0
