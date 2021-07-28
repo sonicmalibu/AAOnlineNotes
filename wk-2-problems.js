ce232a8769704c806bc7ab2d68853aaab0d22d75
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-28 11:20:12 -0500

    average value for each

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -11,3 +11,22 @@
 // console.log(sumForEach([4, 3, -1, 10])); // 16
 // console.log(sumForEach([6, 7, 2])); // 15
 // console.log(sumForEach([])); // 0
+
+
+
+// Average Value For Each*********************************************************************************************
+// function avgVal(arr) {
+//     let sum = 0;
+//     arr.forEach(function(num) {
+//         sum += num;
+//     });
+//     let average = sum / arr.length;
+//     if (arr.length === 0) {
+//         return null;
+//     }
+//     return average;
+// }
+
+// console.log(avgVal([5, 10])); // 7.5
+// console.log(avgVal([3, 7, 2, 1, 2])); // 3
+// console.log(avgVal([])); // null
