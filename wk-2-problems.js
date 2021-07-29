9d4b9bce9b715b9afdbda37ca966825e331e0324
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-29 18:49:06 -0500

    cat builder

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -351,7 +351,7 @@
 
 
 
-// Two D Product***********************************************************************************************************************
+// Two Dimensional Product For Each***********************************************************************************************************************
 // function twoDimensionalProduct(arr) {
 //     let product = 1;
 //     arr.forEach(function(subArr) {
@@ -374,3 +374,21 @@
 //     [2]
 //   ];
 //   console.log(twoDimensionalProduct(arr2)); // 88
+
+
+
+// Cat Builder***************************************************************************************
+// function catBuilder(name, color, toys) {
+//     let cat = {
+//         name: name,
+//         color: color,
+//         toys: toys,
+//     };
+//     return cat;
+// }
+
+// console.log(catBuilder("Whiskers", "black", ["scratching-post", "yarn"]));
+// // prints: { name: 'Whiskers', color: 'black', toys: ['scratching-post', 'yarn'] }
+
+// console.log(catBuilder("Nyan", "rainbow", ["poptarts"]));
+// // prints: { name: 'Nyan', color: 'rainbow', toys: [ 'poptarts' ] }
