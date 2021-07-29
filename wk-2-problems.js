4ce9a790ff52716969486d04a781061f7a881631
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-29 14:30:13 -0500

    commenting out temporarily

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -289,10 +289,10 @@
 
 
 // Hipsterfy******************************************************************************************
-function hipsterfy(sentence) {
-        
-}
+// function hipsterfy(sentence) {
 
-console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
-console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
-console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+// }
+
+// console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+// console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+// console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
