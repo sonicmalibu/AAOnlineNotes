e7c6d4832a79f748b4cc960aebd348895d9df6c2
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-07-30 12:26:35 -0500

    Choosey Endings

--- wk-1-problems.js
+++ wk-1-problems.js
@@ -1292,3 +1292,39 @@
 
 // console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
 // console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
+
+
+
+// Choosey Endings********************************************************************************************************************
+// let chooseyEndings = function(words, suffix) {
+//     if (!Array.isArray(words)) {
+//       return [];
+//     }
+
+//     let filteredWords = [];
+
+//     for (let i = 0; i < words.length; i++) {
+//       let word = words[i];
+//       if (word.endsWith(suffix)) {
+//         filteredWords.push(word);
+//       }
+//     }
+
+//     return filteredWords;
+//   };
+
+
+// console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'ly'));
+// // [ 'family', 'fly', 'timidly' ]
+
+// console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'nd'));
+// // [ 'hound', 'bond' ]
+
+// console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
+// // [ 'simplicity', 'felicity' ]
+
+// console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
+// // [ ]
+
+// console.log(chooseyEndings(17, 'ily'));
+// // [ ]
