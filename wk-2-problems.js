4c44cf0a6e159a1a11dc5828a39dcbc358349547
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-02 17:10:58 -0500

    apple counter

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -527,3 +527,35 @@
 // let object2 = {name: 'Two', location: 'SF'};
 // console.log(valuePair(object1, object2, 'location')); // => [ 'NY', 'SF' ]
 // console.log(valuePair(object1, object2, 'name')); // => [ 'One', 'Two' ]
+
+
+
+// Apple Counter****************************************************************************************
+// function appleCounter(appleObj) {
+//     let counter = 0;
+//     for (let key in appleObj) {
+//         key = key.toLowerCase();
+//         if (key.indexOf('apple') > -1) {
+//             counter += 1;
+//         }
+//     }
+//     return counter;
+// }
+
+// let obj = { banana: "yay!" };
+// console.log(appleCounter(obj)); // => 0
+
+// let obj1 = { crabapple: "yum!" };
+// console.log(appleCounter(obj1)); // => 1
+
+// let obj2 = { crabapple: "yum!", honeyapple: "super yum", banana: "yay" };
+// console.log(appleCounter(obj2)); // => 2
+
+
+// let obj3 = {
+//   crabApple: "yum!",
+//   honeyApple: "super yum",
+//   banana: "yay",
+//   bigapple: "NYC"
+// };
+// console.log(appleCounter(obj3)); // => 3
