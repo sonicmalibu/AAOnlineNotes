ee1e8e7a9f75ffe22199dee778216669876e39e3
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 14:10:33 -0500

    my some

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -809,3 +809,32 @@
 //     return s.toLowerCase().includes('a');
 // };
 // console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
+
+
+
+// My Some*************************************************************************************************************************
+function mySome(array, cb) {
+    array.forEach(function(ele, i, array) {
+        if (cb(ele, i)) {
+            return true;
+        }
+    })
+    return false;
+
+}
+
+
+let result1 = mySome([5, 1, 7, 9], function(ele, i) {
+    return ele === i;
+});
+console.log(result1);   // true
+
+let result2 = mySome([5, 3, 7, 9], function(ele, i) {
+    return ele === i;
+});
+console.log(result2);   // false
+
+let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function(ele) {
+    return ele.length === 4;
+});
+console.log(result3);   // true
