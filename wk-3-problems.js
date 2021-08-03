e54d172ef921de2e682b8c71bd86d10fa2b80f9a
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 15:12:07 -0500

    Sum To N

---
+++ wk-3-problems.js
@@ -0,0 +1,25 @@
+// addLib Problem************************************************************************************************
+// function makeAddLib(verb, adj, noun) {
+//     return `I shall ${verb} to the ${adj} ${noun}?`;
+//   }
+
+//   console.log(makeAddLib("swim", "sparkly", "rainbow")); // => "I shall swim to the sparkly rainbow?"
+//   console.log(makeAddLib("RUN", "FANCY", "ParK")); // => "I shall RUN to the FANCY ParK?"
+
+
+
+// Sum To N*******************************************************************************************************
+// function sumToN(n) {
+// if (n < 0) return null;
+// if (n <= 1) return n;
+// return n + sumToN(n - 1);
+// }
+
+// console.log(sumToN(5)) // returns 15
+// console.log(sumToN(1))  // returns 1
+// console.log(sumToN(9))  // returns 45
+// console.log(sumToN(-8))  // returns null
+
+
+
+//
