54aed80a616acfa6fb81f36338dee9adfcc35205
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 10:33:57 -0500

    multi map

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -698,3 +698,28 @@
 //     return el.toUpperCase() + '!';
 // });
 // console.log(result2);   // [ 'RUN!', 'FORREST!' ]
+
+
+
+// Multi-Map***************************************************************************************************************
+// function multiMap(val, n, cb) {
+//     for (let i = 0; i < n; i++) {
+//         val = cb(val);
+//     }
+//     return val;
+// }
+
+// let result1 = multiMap(7, 2, function(n) {
+//     return n * 10;
+//   });
+//   console.log(result1); // 700
+
+//   let result2 = multiMap(7, 3, function(n) {
+//       return n * 10;
+//   });
+//   console.log(result2); // 7000
+
+//   let result3 = multiMap("hi", 5, function(s) {
+//     return s + "!";
+//   });
+//   console.log(result3); // hi!!!!!
