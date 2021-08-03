c13d07906efa0071546dd790435a5c12c0433b42
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 14:15:44 -0500

    My Some

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -813,28 +813,33 @@
 
 
 // My Some*************************************************************************************************************************
-function mySome(array, cb) {
-    array.forEach(function(ele, i, array) {
-        if (cb(ele, i)) {
-            return true;
-        }
-    })
-    return false;
-
-}
+// function mySome(array, cb) {
+//     for (let i = 0; i < array.length; i++) {
+//         let ele = array[i];
 
 
-let result1 = mySome([5, 1, 7, 9], function(ele, i) {
-    return ele === i;
-});
-console.log(result1);   // true
+//         if (cb(ele, i)) {
+//             return true;
+//         }
+//     }
+//     return false;
 
-let result2 = mySome([5, 3, 7, 9], function(ele, i) {
-    return ele === i;
-});
-console.log(result2);   // false
+// }
 
-let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function(ele) {
-    return ele.length === 4;
-});
-console.log(result3);   // true
+
+// let result1 = mySome([5, 1, 7, 9], function (ele, i) {
+//     return ele === i;
+// });
+// console.log(result1);   // true
+
+// let result2 = mySome([5, 3, 7, 9], function (ele, i) {
+//     return ele === i;
+// });
+// console.log(result2);   // false
+
+// let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function (ele) {
+//     return ele.length === 4;
+// });
+// console.log(result3);   // true
+
+
