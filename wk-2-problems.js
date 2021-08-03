60f2e5bc1a5b2076c80fa67bdfb32b7a606ad844
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 14:22:53 -0500

    count

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -843,3 +843,34 @@
 // console.log(result3);   // true
 
 
+
+// Count**********************************************************************************************************************
+// function count(array, cb) {
+//     let counter = 0;
+//     array.forEach(function(num) {
+//         if (cb(num)) {
+//             counter++;
+//         }
+//     })
+//     return counter;
+// }
+
+// let result1 = count([18, 5, 32, 7, 100], function (n) {
+//     return n % 2 === 0;
+// });
+// console.log(result1); // 3
+
+// let result2 = count([17, 5, 31, 7, 100], function (n) {
+//     return n % 2 === 0;
+// });
+// console.log(result2); // 1
+
+// let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+//     return str.includes('o');
+// });
+// console.log(result3); // 3
+
+// let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+//     return str.includes('x');
+// });
+// console.log(result4); // 0
