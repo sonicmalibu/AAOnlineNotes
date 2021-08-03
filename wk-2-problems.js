f04b2da5b5eba7b1064b3b1565bfd3e575b560c4
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 14:37:26 -0500

    chain map

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -874,3 +874,31 @@
 //     return str.includes('x');
 // });
 // console.log(result4); // 0
+
+
+
+// Chain Map***************************************************************************************************************
+// function chainMap(val, ...cb) {
+//     cb.forEach(function(cb) {
+//         val = cb(val);
+//     })
+//     return val;
+// }
+
+// let add5 = function(n) {
+//     return n + 5;
+// };
+
+// let half = function(n) {
+//     return n / 2;
+// };
+
+// let square = function(n) {
+//     return n * n;
+// };
+
+// console.log(chainMap(25, add5));                // 30
+// console.log(chainMap(25, add5, half));          // 15
+// console.log(chainMap(25, add5, half, square));  // 225
+// console.log(chainMap(4, square, half));         // 8
+// console.log(chainMap(4, half, square));         // 4
