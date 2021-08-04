1e75aefac42344f0184fa964f02c24da4a1af43b
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 21:28:15 -0500

    xor select

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -1104,3 +1104,38 @@
 // };
 // console.log(suffixCipher('incremental progress is very instrumental', cipher2));
 // // INCREMENTAL progressth isth very INSTRUMENTAL
+
+
+
+// XOR Select*****************************************************************************************************************
+// let xorSelect = function(array, cb1, cb2) {
+//     let selected = array.filter(function(el) {
+//       return (cb1(el) || cb2(el)) && !(cb1(el) && cb2(el));
+//     });
+//     return selected;
+//   };
+
+//   let isEven = function(n) {
+//     return n % 2 === 0;
+//   };
+
+//   let isPositive = function(n) {
+//     return n > 0;
+//   };
+
+//   console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+//   // [ -2, 1, 3 ]
+
+
+//   let longString = function(s) {
+//     return s.length > 4;
+//   };
+
+//   let startsA = function(s) {
+//     return s[0] === "a";
+//   };
+
+//   console.log(
+//     xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+//   );
+//   // [ 'art', 'app', 'buttery' ]
