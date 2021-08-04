fd0893ea31c5f11846cde0e49e491be25f2ea2dd
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-04 11:37:30 -0500

    alternating map

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -1297,3 +1297,40 @@
 //     return s.length === 3;
 // });
 // console.log(result3); // -1
+
+
+
+// Alternating Map***********************************************************************************************************
+// function alternatingMap(array, cb1, cb2) {
+//     let arr = [];
+//     array.forEach(function(ele, i, _array) {
+//         if (i % 2 === 0) {
+//                 arr.push(cb1(ele));
+//         } else {
+//             arr.push(cb2(ele));
+//         }
+//     })
+//     return arr;
+// }
+
+
+// let triple = function (n) {
+//     return 3 * n;
+// };
+
+// let half = function (n) {
+//     return n / 2;
+// };
+// console.log(alternatingMap([7, 3, 2, 9, 8], triple, half));
+// // [ 21, 1.5, 6, 4.5, 24 ]
+
+
+// let yell = function (s) {
+//     return s.toUpperCase() + '!';
+// };
+
+// let whisper = function (s) {
+//     return '..' + s.toLowerCase() + '..';
+// };
+// console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper));
+// // [ 'HEY!', '..everyone..', 'WHATS!', '..up??..' ]
