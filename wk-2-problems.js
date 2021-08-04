42d64cfb2a02d921f5f82758bb47a7a1fd4658c9
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-04 10:41:16 -0500

    first index

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -1269,3 +1269,31 @@
 // console.log(atMost(['boat', 'cat', 'car'], 1, startsWithA));                    // true
 // console.log(atMost(['boat', 'cat', 'car', 'academy'], 1, startsWithA));         // true
 // console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  // false
+
+
+
+// First Index*****************************************************************************************************************
+// function firstIndex(array, cb) {
+//     for (let i = 0; i < array.length; i++) {
+//         let ele = array[i];
+//         if (cb(ele)) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// let result1 = firstIndex([3, 7, 8, 10], function (n) {
+//     return n % 2 === 0;
+// });
+// console.log(result1); // 2
+
+// let result2 = firstIndex(['dog', 'cat', 'tree'], function (s) {
+//     return s.length === 3;
+// });
+// console.log(result2); // 0
+
+// let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
+//     return s.length === 3;
+// });
+// console.log(result3); // -1
