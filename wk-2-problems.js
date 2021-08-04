a555ea1e15a1f49d0cef7e9e22df0398603e0223
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-03 21:36:26 -0500

    greater callback value

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -1183,3 +1183,30 @@
 //     return el.length === idx;
 // });
 // console.log(result6);   // true
+
+
+
+// Greater Callback Value********************************************************************************************************
+// function greaterCallbackValue(val, cb1, cb2) {
+//     let res1 = cb1(val);
+//     let res2 = cb2(val);
+
+//     if (res1 > res2) {
+//         return res1;
+//     } else {
+//         return res2;
+//     }
+// }
+
+
+// let doubler = function (n) {
+//     return 2 * n;
+// }
+
+// let squarer = function (n) {
+//     return n * n;
+// }
+
+// console.log(greaterCallbackValue(5, doubler, squarer));     // 25
+// console.log(greaterCallbackValue(1, doubler, squarer));     // 2
+// console.log(greaterCallbackValue(9, Math.sqrt, doubler));   // 18
