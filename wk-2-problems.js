3c5c451a639012417635af877356a53da39d35c2
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-04 12:08:59 -0500

    My Simple Reduce

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -1338,25 +1338,30 @@
 
 
 // My Simple Reduce*****************************************************************************************************************
-function mySimpleReduce(array, cb) {
-    
-}
+// function mySimpleReduce(array, cb) {
+//     accum = array[0];
+//     let sliced = array.slice(1);
+//     sliced.forEach(function(ele) {
+//         accum = cb(accum, ele);
+//     })
+//     return accum;
+// }
 
-let result1 = mySimpleReduce([5, 3, 2, 4], function(sum, el) {
-    return sum + el;
-});
-console.log(result1); // 14
+// let result1 = mySimpleReduce([5, 3, 2, 4], function(sum, el) {
+//     return sum + el;
+// });
+// console.log(result1); // 14
 
-let result2 = mySimpleReduce([4, 6, 2], function(product, el) {
-    return product * el;
-});
-console.log(result2); // 48
+// let result2 = mySimpleReduce([4, 6, 2], function(product, el) {
+//     return product * el;
+// });
+// console.log(result2); // 48
 
-let result3 = mySimpleReduce([4, 6, 2, 8, 3], function(max, el) {
-    if (el > max) {
-        return el;
-    } else {
-        return max;
-    }
-});
-console.log(result3); // 8
+// let result3 = mySimpleReduce([4, 6, 2, 8, 3], function(max, el) {
+//     if (el > max) {
+//         return el;
+//     } else {
+//         return max;
+//     }
+// });
+// console.log(result3); // 8
