ac78a7ccb4f04a3a54d8bb8b3ad0e0fecec7ec76
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-08-12 15:08:34 -0500

    recure

--- notes.js
+++ notes.js
@@ -697,3 +697,19 @@ and how many odds are in my array */
 //     console.log(num);
 //     countdown(num - 1);
 // }
+
+
+function echo(word) {
+    if (word.length > 1) {
+        console.log(word.toUpperCase());
+        let response = echo(word.slice(1));
+
+        console.log(response);
+        return word;
+    } else {
+        console.log(word.toUpperCase());
+        return word;
+    }
+}
+
+echo('howdy');
