c51becb1995fffbae52ae2ed4aa272ab68fe519f
Shawn Williams <shawnwilliams.affiliate@gmail.com>
2021-09-02 10:13:39 -0500

    arrow additives

--- wk-2-problems.js
+++ wk-2-problems.js
@@ -1366,3 +1366,13 @@
 //     }
 // });
 // console.log(result3); // 8
+
+
+
+// Arrow Additive***********************************************************************************
+
+// let addFive = num => num + 5;
+
+// let result1 = addFive(0); // returns 5
+// let result2 = addFive(10); // returns 15
+// let result3 = addFive(37); // returns 42
